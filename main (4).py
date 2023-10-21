8ebcb70272dac9dcbbb0a95fd0b8b7737809058b
Karpagam2005 <144587151+Karpagam2005@users.noreply.github.com>
2023-10-21 20:15:39 +0530

    Add files via upload

--- main (4).py
+++ main (4).py
@@ -1,18 +1,9 @@
-def linear_search_product(products, target_product):
-    indices = []
-    
-    for i, product in enumerate(products):
-        if product == target_product:
-            indices.append(i)
-    
-    return indices
-
-# Example usage:
-products = ["apple", "banana", "apple", "orange", "apple", "grape"]
-target = "apple"
-
-result = linear_search_product(products, target)
-if result:
-    print(f"The product '{target}' was found at indices: {result}")
-else:
-    print(f"The product '{target}' was not found in the list.")
+factorial = 1 
+ if num < 0: 
+   print(" Factorial does not exist for negative numbers") 
+ elif num == 0: 
+   print("The factorial of 0 is 1") 
+ else: 
+   for i in range(1, num + 1): 
+     factorial = factorial * i 
+   print("The factorial of", num, "is", factorial)
